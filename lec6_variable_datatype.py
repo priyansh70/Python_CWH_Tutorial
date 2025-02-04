c0a4cad9e7500d394643a25661e02e6cdf65d938
priyansh70 <priyansh70890@gmail.com>
2025-02-04 17:14:29 +0530

    Complete Lecture 6

--- lec6_variable_datatype.py
+++ lec6_variable_datatype.py
@@ -9,3 +9,42 @@ print(b)
 
 # DataType: It specifies the type of value a variable holds. This is required in programming to do various operations without causing error.
 # We can print the type of any operator using type function
+
+c = True
+print(c)
+
+d = None 
+print(d)
+
+e = 1.1
+print(e)
+
+f = complex(8,2)
+print(f)
+
+# Types 
+print("The Type of a is: ",type(a))
+print("The Type of b is: ",type(b))
+print("The Type of c is: ",type(c))
+print("The Type of d is: ",type(d))
+print("The Type of e is: ",type(e))
+print("The Type of f is: ",type(f))
+
+# List 
+test_list = [1,3,4,"priyansh"]
+print(test_list)
+print("The Type of test_list is: ",type(test_list))
+
+
+# Tuple 
+test_tuple = ("priyansh","patel")
+print(test_tuple)
+print("The Type of test_tuple is: ",type(test_tuple))
+
+# Dictionary 
+test_dict = {
+    "name" : "priyansh",
+    "age" : 21
+}
+print(test_dict)
+print("The Type of test_dict is: ",type(test_dict))
